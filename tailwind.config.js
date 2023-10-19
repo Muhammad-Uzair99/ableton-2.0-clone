32304ca93fa3895239da068819111727477b932c
Muhammad-Uzair99 <uzairmaster97@gmail.com>
2023-10-19 22:44:50 +0500

    Add starter files and installed Tailwinf CSS with custome theme

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'text': '#f8e0dd',
+        'background': '#0d0403',
+        'primary': '#ff4632',
+        'secondary': '#1e0806',
+        'accent': '#c83828',
+       },       
+    },
+  },
+  plugins: [],
+}
+
