ecae019bac096450b20c9ce24caa653eeb067216
Muhammad-Uzair99 <uzairmaster97@gmail.com>
2023-10-21 21:57:48 +0500

    Styled header & primary Nav for large screen

--- js/header/primary-nav.js
+++ js/header/primary-nav.js
@@ -8,12 +8,29 @@ export default function PrimaryNav () {
     textColorToggler();
     svgFillToggler();
     navContentDispalyer();
+
+    moreBtnPlusMinusToggler();
+    navContentDispalyerLgScreen();
+  });
+
+  const moreBtnEl = document.querySelector("#more-btn");
+
+  moreBtnEl.addEventListener("click", () => {
+    moreBtnPlusMinusToggler();
+    navContentDispalyerLgScreen();
+
+    menuBtnArrowToggler();
+    textColorToggler();
+    svgFillToggler();
+    navContentDispalyer();
   });
   
-  return menuBtnEl;
+  return moreBtnEl;
 }
 
-function menuBtnArrow() {
+// Screen size < 1024px Primary Nav Code
+
+function menuBtnArrowToggler() {
   const arrowUpDownElClasses = document.querySelector("#arrow-up-down").classList;
   
   arrowUpDownElClasses.toggle("-rotate-180");
@@ -40,4 +57,19 @@ function navContentDispalyer() {
   const primaryNavContentElClasses = document.querySelector("#primary-nav-content").classList;
 
   primaryNavContentElClasses.toggle("-translate-y-full");
+}
+
+// Large Screen Primary Nav Code
+
+function moreBtnPlusMinusToggler() {
+  const moreBtnClasses = document.querySelector("#more-btn").classList;
+
+  moreBtnClasses.toggle("after:content-['+']");
+  moreBtnClasses.toggle("after:content-['-']")
+}
+
+function navContentDispalyerLgScreen() {
+  const moreNavContentElClasses = document.querySelector("#more-nav-content").classList;
+
+  moreNavContentElClasses.toggle("lg:hidden");
 }
